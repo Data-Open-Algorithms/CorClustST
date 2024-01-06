6cd905f2dc3ffa8e934ac4835d3f3039187ab74b
Ro .- <rodmendezp@gmail.com>
2024-01-06 13:31:57 -0800

    base setup and gitignore

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='clust_st',
+    version='0.0.1',
+    author='Data-Open-Algorithm',
+    description='Spatio Temporal Clusters',
+    url='https://github.com/Data-Open-Algorithms/clust_st',
+    packages=find_packages(),
+)
