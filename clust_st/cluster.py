5883de5baeb48d84d3c81f03d2ca76d4de365b33
Pascal Sigel <pascal.sigel@gmail.com>
2024-01-14 18:02:33 -0300

    init file

---
+++ clust_st/cluster.py
@@ -0,0 +1,12 @@
+class CorClustST:
+    def __init__(self, epsilon: float, rho: float) -> None:
+        self.epsilon = epsilon
+        self.rho = rho
+        self.labels_ = None
+
+    def step_1(self, distance_matrix, corr_matrix):
+
+
+
+    def fit(self, dist_matrix, corr_matrix):
+        self.step_1(dist_matrix, corr_matrix)
