10e582535a2d561b10f4ccb35bc0793b5ba690c1
Pascal Sigel <pascal.sigel@gmail.com>
2024-01-14 19:21:09 -0300

    creating matrix of distance and correlation for sample clustering_raining_data_chile.py

--- samples/clustering_raining_data_chile.py
+++ samples/clustering_raining_data_chile.py
@@ -1,42 +1,43 @@
+import os.path
 import unittest
 import numpy as np
 from clust_st import CorClustST
+import pandas as pd
 
-expected_clusters = [
-    '5100006',
-    '5427008',
-    '360011',
-    '340031',
-    '10312001',
-    '4502005',
-    '8367001',
-    '1020017',
-    '4554003',
-    '3815004',
-    '10350001',
-    '3431004',
-    '10904005',
-    '1730020',
-    '450005',
-    '2101003',
-    '520006'
-]
+try:
+    from sklearn.neighbors import DistanceMetric
+except ImportError:
+    from sklearn.metrics import DistanceMetric
+
+
+def get_distance_matrix(stations_info_df, stations_of_interest):
+    dist_func = DistanceMetric.get_metric('haversine')
+    df_spatial_sample = stations_info_df.loc[stations_of_interest][["latitud", "longitud"]]
+    df_spatial_sample_rad = df_spatial_sample.copy()
+    df_spatial_sample_rad['latitud'] = np.radians(df_spatial_sample_rad['latitud'])
+    df_spatial_sample_rad['longitud'] = np.radians(df_spatial_sample_rad['longitud'])
+    dist_matrix = dist_func.pairwise(df_spatial_sample_rad[['latitud', 'longitud']].to_numpy()) * 6373
+    return dist_matrix
+
 
 def main():
-    # download data
+    print("hola")
+    # data load
+    precipitations_df = pd.read_csv("./chile_precipitation_clustering_data/chilean_stations_precipitation.csv",
+                                    index_col=0, parse_dates=True)
+    stations_info_df = pd.read_csv("./chile_precipitation_clustering_data/stations_info.csv",
+                                   index_col=1)
+    stations_of_interest = pd.Series(precipitations_df.columns.values).apply(int).values
 
     # create distance matrix
-    dist_matrix = None
+    dist_matrix = get_distance_matrix(stations_info_df, stations_of_interest)
 
     # create correlation matrix
-    corr_matrix = None
+    corr_matrix = abs(precipitations_df.corr(method="spearman"))
 
     corclust_st = CorClustST(epsilon=1.7, rho=2.3)
     corclust_st.fit(dist_matrix, corr_matrix)
-    cluster_centers = corclust_st.cluster_centers_
-    # labels = corclust_st.labels_
-    expected_center = np.array(expected_clusters)
-    np.testing.assert_array_equal(expected_center, cluster_centers.index)
+
     # plots
 
 
