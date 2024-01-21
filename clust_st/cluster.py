34f3574a17c44d10fd137fc81eef9f5f91958cce
Pascal Sigel <pascal.sigel@gmail.com>
2024-01-21 18:09:39 -0300

    step 0 complete

--- clust_st/cluster.py
+++ clust_st/cluster.py
@@ -2,29 +2,31 @@ from scipy.stats import rankdata
 import numpy as np
 
 class CorClustST:
-    def __init__(self, epsilon: float, rho: float) -> None:
+    def __init__(self, epsilon: float, rho: float, free_neigh_ratio: float = 0.5) -> None:
         self.epsilon = epsilon
         self.rho = rho
         self.labels_ = None
         self.neigh_amount_initial_ = None
+        self.st_neigh = None
+        self.free_neigh_ratio = free_neigh_ratio
 
     def step_0(self, distance_matrix, corr_matrix):
         corr_matrix = corr_matrix.values
         corr_neigh = corr_matrix > abs(self.rho)
         spat_neigh = distance_matrix < self.epsilon
-        st_neigh = corr_neigh & spat_neigh
-        self.neigh_amount_initial_ = st_neigh.sum(axis=0)
+        self.st_neigh = corr_neigh & spat_neigh
+
+        # self.st_neigh = self.st_neigh - np.eye(corr_matrix.shape[0]) # ToDo : think about adding this line
+
+        self.neigh_amount_initial_ = self.st_neigh.sum(axis=0)
         self.labels_ = np.ones((corr_matrix.shape[0],)) * -1
 
     def step_1(self, distance_matrix, corr_matrix):
-        corr_matrix = corr_matrix.values
-        corr_neigh = corr_matrix > abs(self.rho)
-        spat_neigh = distance_matrix < self.epsilon
-        st_neigh = corr_neigh & spat_neigh
-        neigh_amount = np.dot(st_neigh, (self.labels_ < 0)*1)
+        free_neigh_amount = np.dot(self.st_neigh, (self.labels_ < 0)*1)
+        more_half_neigh_available = np.divide(free_neigh_amount, self.neigh_amount_initial_) > self.free_neigh_ratio
 
-        more_half_neigh_available = np.divide(neigh_amount, self.neigh_amount_initial_) > 0.5
-        # for candidate
+
+
 
     def fit(self, dist_matrix, corr_matrix):
         self.step_0(dist_matrix, corr_matrix)
